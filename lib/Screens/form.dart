75880268afd984071a40bf1df5935203239998a7
budhiparkash <shivam.dev389@gmail.com>
2024-01-09 18:24:55 +0530

    form valid

--- lib/Screens/form.dart
+++ lib/Screens/form.dart
@@ -2,6 +2,10 @@
 import 'package:first_project/Screens/details.dart';
 import 'package:flutter/material.dart';
 
+
+
+enum ProductTypeEnum {Donwloadable , Deliverable}
+
 class MyForm extends StatefulWidget {
   const MyForm({super.key});
 
@@ -14,10 +18,21 @@ class MyForm extends StatefulWidget {
 
 class _MyFormState extends State<MyForm> {
 
+
+  _MyFormState(){
+    _selectedVal = _productSizeList[0];
+  }
+
   var _productName ="";
   final _productController = TextEditingController();
    bool? _checkbox = false;
    bool? _listtileCheckBox = false;
+   ProductTypeEnum? _productTypeEnum;
+
+   final _productSizeList = ["Small" , "Medium" , "Large" , "X - Large"];
+   String? _selectedVal = "Small";
+
+   final _formKey = GlobalKey<FormState>();
 
   void _updateText(){
     setState(() {
@@ -49,32 +64,48 @@ class _MyFormState extends State<MyForm> {
         padding: EdgeInsets.all(20.0),
         child: ListView(
           children: [
-
-            TextFormField(
-              controller: _productController,
-              /*onChanged: (val){
+            Form(child: Column(
+              key: _formKey,
+              children: [
+                TextFormField(
+                  validator: (value){
+                    if(value == null || value.isEmpty){
+                     return "Please enter Product Name";
+                    }else{
+                      return null;
+                    }
+                  },
+                  controller: _productController,
+                  /*onChanged: (val){
                 _updateText(val);
               },*/
-              decoration: InputDecoration(
-                labelText: "Product Name",
-                prefixIcon: Icon(Icons.verified_user_rounded),
-                  border: OutlineInputBorder()
-              ),
-            ),
-            SizedBox(height: 20.0),
+                  decoration: InputDecoration(
+                      labelText: "Product Name",
+                      prefixIcon: Icon(Icons.verified_user_rounded),
+                      border: OutlineInputBorder()
+                  ),
+                ),
+                SizedBox(height: 20.0),
 
-            TextFormField(
-              decoration: InputDecoration(
-                  labelText: "Description",
-                prefixIcon: Icon(Icons.verified_outlined),
-                border: OutlineInputBorder()
-              ),
-            ),
-            //Text("Product Name is ${_productController.text}")
-            SizedBox(height: 20.0),
+                TextFormField(
+                  validator: (value){
+                    if(value == null || value.isEmpty){
+                      return "Please enter Product Name";
+                    }else{
+                      return null;
+                    }
+                  },
+                  decoration: InputDecoration(
+                      labelText: "Description",
+                      prefixIcon: Icon(Icons.verified_outlined),
+                      border: OutlineInputBorder()
+                  ),
+                ),
+                //Text("Product Name is ${_productController.text}")
+                SizedBox(height: 20.0),
 
 
-            Checkbox(
+                /* Checkbox(
               checkColor: Colors.white,
                 activeColor: Colors.purple,
                 tristate: true,
@@ -83,38 +114,128 @@ class _MyFormState extends State<MyForm> {
               setState(() {
                 _checkbox = val;
               });
-            }),
+            }),*/
 
-            CheckboxListTile(value: _listtileCheckBox,
-                title: Text("Top Product"),
-                controlAffinity: ListTileControlAffinity.leading,
+                CheckboxListTile(value: _listtileCheckBox,
+                    title: Text("Top Product"),
+                    controlAffinity: ListTileControlAffinity.leading,
+                    onChanged: (val){
+                      setState(() {
+                        _listtileCheckBox = val;
+                      });
+
+                    }),
+                /*  Radio(value: ProductTypeEnum.Deliverable,
+                groupValue: _productTypeEnum,
                 onChanged: (val){
               setState(() {
-                _listtileCheckBox = val;
+                _productTypeEnum = val;
               });
 
-            }),
-            Radio(value: null, groupValue: null, onChanged: null),
+            }),*/
 
-            RadioListTile(value: null, groupValue: null, onChanged: null),
+                RadioListTile(value: ProductTypeEnum.Deliverable,
+                    contentPadding: EdgeInsets.all(0.0),
+                    title: Text(ProductTypeEnum.Deliverable.name),
+                    tileColor: Colors.deepPurple.shade50,
+                    groupValue: _productTypeEnum,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5.0)
+                    ),
+                    dense: true,
+                    onChanged: (val){
+                      setState(() {
+                        _productTypeEnum = val;
+                      });
+
+                    }),
+
+                SizedBox(height: 10.0),
+
+                RadioListTile(value: ProductTypeEnum.Donwloadable,
+                    contentPadding: EdgeInsets.all(0.0),
+                    title: Text(ProductTypeEnum.Donwloadable.name),
+                    groupValue: _productTypeEnum,
+                    dense: true,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5.0)
+                    ),
+                    tileColor: Colors.deepPurple.shade50,
+                    onChanged: (val){
+                      setState(() {
+                        _productTypeEnum = val;
+                      });
+
+                    }),
+
+                SizedBox(height: 10.0),
+
+                /* DropdownButton(items: _productSizeList.map(
+                (e) => DropdownMenuItem(child: Text(e) , value: e,)
+            ).toList(),
+                onChanged: (val){
+              setState(() {
+                _selectedVal = val as String;
+              });
+                }),*/
+
+                DropdownButtonFormField(
+                    icon: const Icon(
+                      Icons.arrow_drop_down_circle,
+                      color: Colors.deepPurple,
+                    ),
+                    dropdownColor: Colors.deepPurple.shade50,
+                    decoration: InputDecoration(
+                        labelText: "Product Size",
+                        prefixIcon: Icon(
+                          Icons.accessibility_new_rounded,
+                          color: Colors.deepPurple,
+                        ),
+                        border: UnderlineInputBorder()
+                    ),
+                    items: _productSizeList.map(
+                            (e) => DropdownMenuItem(child: Text(e) , value: e,)
+                    ).toList(),
+                    onChanged: (val){
+                      setState(() {
+                        _selectedVal = val as String;
+                      });
+                    }),
 
 
-            myBtn(context)
+
+                SizedBox(height: 10.0),
+                myBtn(context)
+              ],
+
+            )),
+
+
           ],
         ) ,
       )
     );
   }
 
+
+
   OutlinedButton myBtn(BuildContext context) {
     return OutlinedButton(
         style: OutlinedButton.styleFrom(minimumSize: const Size(200,50)),
         onPressed: (){
-          Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context){
-                return Details(productName: _productController.text,);
-          }));
+          if(_formKey.currentState!.validate()){
+            ScaffoldMessenger.of(context).showSnackBar((
+            const SnackBar(content: Text("Processing Data"))
+            ));
+
+          }else{
+            Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context){
+                  return Details(productName: _productController.text,);
+                }));
+          }
+
 
         },
         child: Text(
